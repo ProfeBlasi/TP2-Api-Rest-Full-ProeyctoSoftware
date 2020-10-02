794f51d4443d98207805db894807a9730feddf7b
Ezequiel Blasi <profeblasi@gmail.com>
2020-10-01 22:58:58 -0300

    AndaTemplete

---
+++ Domain/Entities/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    public class Cliente
+    {
+        [Key][Required]
+        public int ClienteId { get; set; }
+        [Column(TypeName = "varchar(10)")]
+        public int DNI { get; set; }
+        [Column(TypeName = "varchar(45)")]
+        public string Nombre { get; set; }
+        [Column(TypeName = "varchar(45)")]
+        public string Apellido { get; set; }
+        [Column(TypeName = "varchar(45)")]
+        public string Email { get; set; }
+    }
+}
---
+++ Domain/Entities/Libros.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Domain.Entities
+{ 
+    public class Libros
+    {
+        [Key][Required][Column(TypeName = "varchar(45)")]
+        public string ISBN { get; set; }
+        [Column(TypeName = "varchar(45)")]
+        public string Titulo { get; set; }
+        [Column(TypeName = "varchar(45)")]
+        public string Autor { get; set; }
+        [Column(TypeName = "varchar(45)")]
+        public string Editorial { get; set; }
+        [Column(TypeName = "varchar(45)")]
+        public string Edicion { get; set; }
+        public int Stock { get; set; }
+        [Column(TypeName = "varchar(45)")]
+        public string Imagen { get; set; }
+    }
+}
